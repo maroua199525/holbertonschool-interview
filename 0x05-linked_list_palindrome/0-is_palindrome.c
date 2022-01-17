817d37c4a1f5252fca802ecdeea55b877c51ed5c
maroua199525 <2646@holbertonschool.com>
2022-01-17 18:44:35 +0100

    add task palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+* is_palindrome -  checks if a singly linked list is a palindrome.
+* @head: pointer to pointer of first node of listint_t list
+* Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
+
+int is_palindrome(listint_t **head)
+{
+	const listint_t *current;
+	unsigned int i; /* number of nodes */
+	unsigned int j, length;
+	int array[MAX];
+
+	current = *head;
+	i = 0;
+	while (current != NULL)
+	{
+		array[i] = current->n;
+		current = current->next;
+		i++;
+	}
+	length = i - 1;
+	for (j = 0; j < (length / 2); j++)
+	{
+		if (array[j] == array[length - j])
+			return (1);
+	}
+	return (0);
+}
