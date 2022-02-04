69fd75a129fda94c53555aebe0c1fdfe26838870
maroua199525 <2646@holbertonschool.com>
2022-02-04 11:21:09 +0100

    add task interview

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - a function in C that checks if a singly linked list has a cycle in it
+ * @list: the given list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *first;
+	listint_t *tmp;
+
+	if (!list)
+		return (0);
+	tmp = list;
+	first = list->next;
+	while (tmp && first && first->next)
+	{
+		tmp = tmp->next;
+		first = first->next->next;
+		if (tmp == first)
+			return (1);
+	}
+	return (0);
+}
