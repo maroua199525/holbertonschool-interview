f76167dfa3737a27dba4bfdd7d6203a79c30bebd
maroua199525 <2646@holbertonschool.com>
2022-04-14 16:05:25 +0100

    fix error: isdigit

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -27,16 +27,18 @@ def search_queens(x, N, board):
                 search_queens(x + 1, N, board)
                 board.remove(pos_qn)
 
+
 if __name__ == '__main__':
+    """search the queens"""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-    if not isinstance(int(sys.argv[1]), int):
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
         print("N must be a number")
-        exit(1)
+        sys.exit(1)
     N = int(sys.argv[1])
     if (N < 4):
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
     lst = []
     search_queens(0, N, lst)
