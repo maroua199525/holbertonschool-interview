22d5a87d1f9b7bd71a0d7e5083484012805fb2ec
maroua199525 <2646@holbertonschool.com>
2022-04-20 00:44:10 +0100

    first commit

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+* heap_sort - a function that sorts an array of integers in ascending
+* order using the Heap sort algorithm
+* @array:an array of integers
+* @size: the size of the array
+* Return: void
+**/
+
+
+void heap_sort(int *array, size_t size)
+{
+    
+}
