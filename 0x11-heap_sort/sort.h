95a7f06dd47b7bc30e68689f35fc1784348f2c66
maroua199525 <2646@holbertonschool.com>
2022-04-20 23:38:00 +0100

    add task heap sort

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -1,2 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
 #include <stdio.h>
 #include <stdlib.h>
+
+void heap_sort(int *array, size_t size);
+void heapify(int *array, int end, int i, size_t size);
+void print_array(const int *array, size_t size);
+void swap(int *a, int *b);
+#endif
