afe9e2426a1493b80ffac9a7642c2854ef3cc8a6
maroua199525 <2646@holbertonschool.com>
2022-06-06 15:02:39 +0100

    fix pep8

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,11 +12,10 @@ def rotate_2d_matrix(matrix):
         for i in range(m, n):
             aux = matrix[i][n]
             matrix[i][n] = matrix[m][i]
-            aux2 = matrix[n][n+ m - i]
-            matrix[n][n+ m - i] = aux
-            aux = matrix[n+ m - i][m]
-            matrix[n+ m - i][m] = aux2
+            aux2 = matrix[n][n + m - i]
+            matrix[n][n + m - i] = aux
+            aux = matrix[n + m - i][m]
+            matrix[n + m - i][m] = aux2
             matrix[m][i] = aux
         m += 1
-        n-= 1
-
+        n -= 1
