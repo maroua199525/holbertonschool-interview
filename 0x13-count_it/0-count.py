2d87b070d386eb8ec389f1a2177f9e23fb77cdc7
maroua199525 <2646@holbertonschool.com>
2022-06-07 10:16:56 +0100

    excute file

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -11,35 +11,37 @@ Javascript should count as javascript, but java should not).
 """
 
 
-def count_words(subreddit, word_list, after='', count={}):
-    """Queries the Reddit API and returns all hot posts
-    of the subreddit"""
+def count_words(subreddit, word_list, after='', words_counting={}):
+    """ Word counting function """
+
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'custom'}
     payload = {'limit': '100', 'after': after}
     response = requests.get(url, headers=headers,
                             params=payload, allow_redirects=False)
 
-    if response.status_code != 200:
+    if not response.status_code == 200:
         return
     data = response.json().get('data')
-    after = data['after']
-    children_list = data['children']
+    after = data.get('after')
+    children_list = data.get('children')
+
     for child in children_list:
-        title = child['data']['title']
+        title = child.get('data').get('title')
         for word in word_list:
-            n = title.lower().split().count(word.lower())
-            if n > 0:
-                if word in count:
-                    count[word] += n
+            ocurrences = title.lower().split().count(word.lower())
+            if ocurrences > 0:
+                if word in words_counting:
+                    words_counting[word] += ocurrences
                 else:
-                    count[word] = n
+                    words_counting[word] = ocurrences
 
     if after is not None:
-        return count_words(subreddit, word_list, after, count)
+        return count_words(subreddit, word_list, after, words_counting)
     else:
-        if not len(count) > 0:
+        if not len(words_counting) > 0:
             return
-        sort = sorted(count.items(), key=lambda kv: (-kv[1], kv[0]))
-        for key, value in sort:
+        iterator = sorted(words_counting.items(),
+                          key=lambda kv: (-kv[1], kv[0]))
+        for key, value in iterator:
             print('{}: {}'.format(key.lower(), value))
