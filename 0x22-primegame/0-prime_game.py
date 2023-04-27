2ffafcbcdb76601da7e157b627b1a68b2e68128a
maroua199525 <2646@holbertonstudents.com>
2023-04-27 10:04:23 +0100

    change file

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Prime Game
 Maria and Ben are playing a game. Given a set of consecutive integers starting
-from 1 up to and including n, they take turns choosing a prime number from the
-set and removing that number and its multiples from the set. The player that
+ 1 up to and including n, they take turns choosing a prime number  the
+set and removing that number and its multiples  the set. The player that
 cannot make a move loses the game.
 """
 
